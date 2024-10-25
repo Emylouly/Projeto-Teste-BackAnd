ae31d917ee02bb16c862c906202e0450c1eab3e1
Emylouly <emilylouisemika@gmail.com>
2024-10-25 11:07:53 -0300

    kakata

--- demo_backend/src/main/java/br/edu/ifba/demo/backend/api/dto/UsuarioDTO.java
+++ demo_backend/src/main/java/br/edu/ifba/demo/backend/api/dto/UsuarioDTO.java
@@ -1,100 +1,93 @@
 package br.edu.ifba.demo.backend.api.dto;
 
-import java.security.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 import br.edu.ifba.demo.backend.api.model.UsuarioModel;
 
 public class UsuarioDTO {
 
-	private long id_usuario;
-	private String login;
-	private String senha;
-	private Timestamp create_at;
-	private Timestamp last_login;
+    private long id_usuario;
+    private String login;
+    private String senha;
+    private LocalDateTime create_at;
+    private LocalDateTime last_login;
 
-	public static UsuarioDTO converter(UsuarioModel usuarioModel) {
-		var usuario = new UsuarioDTO();
-		usuario.setId_usuario( usuarioModel.getId_usuario() );
-		usuario.setLogin( usuarioModel.getLogin() );
-		usuario.setSenha( usuarioModel.getSenha() );
-		usuario.setCreate_at( usuarioModel.getCreate_at() );
-		usuario.setLast_login( usuarioModel.getLast_login() );
-		return usuario;
-	}
-	
-	public static List<UsuarioDTO> converter(List<UsuarioModel> usuarios) {
-		List<UsuarioDTO> list=new ArrayList<UsuarioDTO>();
-		
-		for (UsuarioModel model : usuarios) {
-			list.add( UsuarioDTO.converter(model)) ;
-		}
-		return list;
-		// return clientes.stream().map( Cliente :: converter).collect(Collectors.toList());
-	}
-	
-	
-	public UsuarioDTO() {
-		super();
-	}
-	
-	
-	
-	public UsuarioDTO(long id_usuario, String login, String senha) {
-		super();
-		this.id_usuario = id_usuario;
-		this.login = login;
-		this.senha = senha;
-	}
+    public static UsuarioDTO converter(UsuarioModel usuarioModel) {
+        var usuario = new UsuarioDTO();
+        usuario.setId_usuario(usuarioModel.getId_usuario());
+        usuario.setLogin(usuarioModel.getLogin());
+        usuario.setSenha(usuarioModel.getSenha());
+        usuario.setCreate_at(usuarioModel.getCreateAt());
+        usuario.setLast_login(usuarioModel.getLastLogin());
+        return usuario;
+    }
 
-	public UsuarioDTO(long id_usuario, String login, String senha, Timestamp create_at, Timestamp last_login) {
-		this.id_usuario = id_usuario;
-		this.login = login;
-		this.senha = senha;
-		this.create_at = create_at;
-		this.last_login = last_login;
-	}
-	
-	public long getId_usuario() {
-		return id_usuario;
-	}
+    public static List<UsuarioDTO> converter(List<UsuarioModel> usuarios) {
+        List<UsuarioDTO> list = new ArrayList<>();
+        for (UsuarioModel model : usuarios) {
+            list.add(UsuarioDTO.converter(model));
+        }
+        return list;
+    }
 
-	public void setId_usuario(long id_usuario) {
-		this.id_usuario = id_usuario;
-	}
+    public UsuarioDTO() {
+        super();
+    }
 
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public UsuarioDTO(long id_usuario, String login, String senha) {
+        super();
+        this.id_usuario = id_usuario;
+        this.login = login;
+        this.senha = senha;
+    }
 
+    public UsuarioDTO(long id_usuario, String login, String senha, LocalDateTime create_at, LocalDateTime last_login) {
+        this.id_usuario = id_usuario;
+        this.login = login;
+        this.senha = senha;
+        this.create_at = create_at;
+        this.last_login = last_login;
+    }
 
-	public Timestamp getCreate_at() {
-		return create_at;
-	}
+    public long getId_usuario() {
+        return id_usuario;
+    }
 
+    public void setId_usuario(long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
 
-	public Timestamp getLast_login() {
-		return last_login;
-	}
+    public String getLogin() {
+        return login;
+    }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
-	public void setCreate_at(Timestamp create_at) {
-		this.create_at = create_at;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public void setLast_login(Timestamp last_login) {
-		this.last_login = last_login;
-	}
+    public LocalDateTime getCreate_at() {
+        return create_at;
+    }
 
-	
+    public void setCreate_at(LocalDateTime create_at) {
+        this.create_at = create_at;
+    }
+
+    public LocalDateTime getLast_login() {
+        return last_login;
+    }
+
+    public void setLast_login(LocalDateTime last_login) {
+        this.last_login = last_login;
+    }
 }
--- demo_backend/src/main/java/br/edu/ifba/demo/backend/api/model/UsuarioModel.java
+++ demo_backend/src/main/java/br/edu/ifba/demo/backend/api/model/UsuarioModel.java
@@ -1,7 +1,6 @@
 package br.edu.ifba.demo.backend.api.model;
 
-import java.security.Timestamp;
-
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,85 +15,79 @@ import lombok.Data;
 @Entity
 @Table(name = "usuario")
 public class UsuarioModel {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_usuario")
-	private long id_usuario;
-	
-	@Column(name = "login", nullable = false, unique = true)
-	private String login;
-	
-	@Column(name = "senha", nullable = false)
-	private String senha;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private long id_usuario;
 
-	@Column(name = "create_at")
-	private Timestamp create_at;
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
 
-	@Column(name = "last_login", nullable = true)
-	private Timestamp last_login;
-    
-	public UsuarioModel() {
-		super();
-	}
+    @Column(name = "senha", nullable = false)
+    private String senha;
 
-	public UsuarioModel(long id_usuario, String login, String senha) {
-		super();
-		this.id_usuario = id_usuario;
-		this.login = login;
-		this.senha = senha;
-	}
+    @Column(name = "create_at", insertable = false, updatable = false)
+    private LocalDateTime create_at;
 
-	
+    @Column(name = "last_login", nullable = true)
+    private LocalDateTime last_login;
 
-	public UsuarioModel(long id_usuario, String login, String senha, Timestamp create_at, Timestamp last_login) {
-		this.id_usuario = id_usuario;
-		this.login = login;
-		this.senha = senha;
-		this.create_at = create_at;
-		this.last_login = last_login;
-	}
+    public UsuarioModel() {
+        super();
+    }
 
-	public long getId_usuario() {
-		return id_usuario;
-	}
+    public UsuarioModel(long id_usuario, String login, String senha) {
+        super();
+        this.id_usuario = id_usuario;
+        this.login = login;
+        this.senha = senha;
+    }
 
-	public void setId_usuario(long id_usuario) {
-		this.id_usuario = id_usuario;
-	}
+    public UsuarioModel(long id_usuario, String login, String senha, LocalDateTime create_at, LocalDateTime last_login) {
+        this.id_usuario = id_usuario;
+        this.login = login;
+        this.senha = senha;
+        this.create_at = create_at;
+        this.last_login = last_login;
+    }
 
-	public String getLogin() {
-		return login;
-	}
+    public long getId_usuario() {
+        return id_usuario;
+    }
 
-	public void setLogin(String login) {
-		this.login = login;
-	}
+    public void setId_usuario(long id_usuario) {
+        this.id_usuario = id_usuario;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public String getLogin() {
+        return login;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
-	public void setCreateAt(Timestamp value) {
-		this.create_at = value;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public Timestamp getCreateAt() {
-		return this.create_at;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	public void setLastLogin(Timestamp value) {
-		this.last_login = value;
-	}
+    public LocalDateTime getCreateAt() {
+        return this.create_at;
+    }
 
-	public Timestamp getLastLogin() {
-		return this.last_login;
-	}
-	
-	
-	
-	
+    public void setCreateAt(LocalDateTime createAt) {
+        this.create_at = createAt;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return this.last_login;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.last_login = lastLogin;
+    }
 }
